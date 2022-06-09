9a15a17c1479eb580aa9559a64a74e6390af2eb6
Alberto Sanchez Hernandez <asanchez@cern.ch>
2022-06-08 20:15:41 -0500

    no evtgen for ups1

---
+++ Quarkonia2022/py8_Ups1SMuMu_CP5_13_6TeV_cfi.py
@@ -0,0 +1,51 @@
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+                         pythiaPylistVerbosity = cms.untracked.int32(0),
+                         filterEfficiency = cms.untracked.double(0.109),
+                         pythiaHepMCVerbosity = cms.untracked.bool(False),
+                         crossSection = cms.untracked.double(1430000.0),
+                         comEnergy = cms.double(13600.0),
+                         maxEventsToPrint = cms.untracked.int32(0),
+                         PythiaParameters = cms.PSet(
+                             pythia8CommonSettingsBlock,
+                             pythia8CP5SettingsBlock,
+                             processParameters = cms.vstring(
+                                'Bottomonium:all = on',                     # Quarkonia, MSEL=62, allow feed-down
+                                '553:onMode = off',                          # Turn off all J/psi decays
+                                '553:onIfMatch = 13 -13',                    # Tun on J/psi -> mu+ mu-
+                                'PhaseSpace:pTHatMin = 7.0'                 # ckin(3), be aware of this
+                             ),
+                             parameterSets = cms.vstring('pythia8CommonSettings',
+                                'pythia8CP5Settings',
+                                'processParameters',
+                             )
+                         ),
+
+)
+
+# Filter any upsilon(NS) with pT > 11 GeV and |eta| < 1.5
+
+mumufilter = cms.EDFilter("PythiaDauVFilter",
+        MotherID = cms.untracked.int32(0),
+        MinPt = cms.untracked.vdouble(4.0,4.0),
+        ParticleID = cms.untracked.int32(553),
+        ChargeConjugation = cms.untracked.bool(False),
+        MinEta = cms.untracked.vdouble(-1.5,-1.5),
+        MaxEta = cms.untracked.vdouble(1.5,1.5),
+        NumberDaughters = cms.untracked.int32(2),
+        DaughterIDs = cms.untracked.vint32(13, -13)
+)
+
+oniafilter = cms.EDFilter("PythiaFilter",
+    Status = cms.untracked.int32(2),
+    MaxRapidity = cms.untracked.double(1.3),
+    MinRapidity = cms.untracked.double(-1.3),
+    MinPt = cms.untracked.double(11.0),
+    ParticleID = cms.untracked.int32(553)
+)
+
+ProductionFilterSequence = cms.Sequence(generator*oniafilter*mumufilter)
+
