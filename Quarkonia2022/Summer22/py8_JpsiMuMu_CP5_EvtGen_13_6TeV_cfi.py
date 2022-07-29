fb9aa526b5ad64854458714e9f5c65c099400f40
Alberto Sanchez Hernandez <asanchez@cern.ch>
2022-07-29 14:04:56 -0500

    cfg using evtgen for decay

---
+++ Quarkonia2022/Summer22/py8_JpsiMuMu_CP5_EvtGen_13_6TeV_cfi.py
@@ -0,0 +1,71 @@
+import FWCore.ParameterSet.Config as cms
+
+from GeneratorInterface.EvtGenInterface.EvtGenSetting_cff import *
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunesRun3ECM13p6TeV.PythiaCP5Settings_cfi import *
+
+generator = cms.EDFilter("Pythia8GeneratorFilter",
+    ExternalDecays = cms.PSet(
+        EvtGen130 = cms.untracked.PSet(
+            convertPythiaCodes = cms.untracked.bool(False),   # required for later versions of evtgen
+            operates_on_particles = cms.vint32(443),          # use EvtGen just for signal particle decay
+            list_forced_decays = cms.vstring('MyJ/psi'),
+            decay_table = cms.string('GeneratorInterface/EvtGenInterface/data/DECAY_2014_NOLONGLIFE.DEC'),
+            particle_property_file = cms.FileInPath('GeneratorInterface/EvtGenInterface/data/evt_2014.pdl'),
+            user_decay_embedded = cms.vstring(
+'''
+Alias MyJ/psi J/psi
+ChargeConj MyJ/psi MyJ/psi
+
+Decay MyJ/psi
+1.0000  mu+        mu-                 PHOTOS VLL ;
+Enddecay
+
+End
+'''
+           )
+        ),
+        parameterSets = cms.vstring('EvtGen130')
+    ),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    comEnergy = cms.double(13600.0),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP5SettingsBlock,
+        processParameters = cms.vstring(
+            'Charmonium:all = on',                       # Quarkonia, MSEL=61, including feed-down
+            'PhaseSpace:pTHatMin = 15.'                  # should bearound 5 GeV below the actual cut
+            ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CP5Settings',
+                                    'processParameters',
+                                    )
+    )
+)
+
+generator.PythiaParameters.processParameters.extend(EvtGenExtraParticles)   # Required by EvtGen
+
+# Filter with high pT cut on dimuon, trying to accomodate trigger requirements.
+
+mumufilter = cms.EDFilter("PythiaDauVFilter",
+        MotherID = cms.untracked.int32(0),
+        MinPt = cms.untracked.vdouble(3.0,3.0),
+        ParticleID = cms.untracked.int32(443),
+        ChargeConjugation = cms.untracked.bool(False),
+        MinEta = cms.untracked.vdouble(-1.6,-1.6),
+        MaxEta = cms.untracked.vdouble(1.6,1.6),
+        NumberDaughters = cms.untracked.int32(2),
+        DaughterIDs = cms.untracked.vint32(13, -13)
+)
+
+oniafilter = cms.EDFilter("PythiaFilter",
+    Status = cms.untracked.int32(2),
+    MaxRapidity = cms.untracked.double(1.3),
+    MinRapidity = cms.untracked.double(-1.3),
+    MinPt = cms.untracked.double(19.0),
+    ParticleID = cms.untracked.int32(443)
+)
+
+ProductionFilterSequence = cms.Sequence(generator*oniafilter*mumufilter)
