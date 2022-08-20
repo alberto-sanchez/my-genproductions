ff38c2b520d2595df0bc1571a91745577504be38
Alberto Sanchez Hernandez <asanchez@cern.ch>
2022-08-20 08:35:33 -0500

    adjusting gun for custom pt and flat eta

--- Quarkonia2022/pguns/PGun_CustomPtEta_JpsiMuMu_CP5_13_6TeV_cfi.py
+++ Quarkonia2022/pguns/PGun_CustomPtEta_JpsiMuMu_CP5_13_6TeV_cfi.py
@@ -4,20 +4,18 @@ from Configuration.Generator.MCTunesRun3ECM13p6TeV.PythiaCP5Settings_cfi import 
 
 generator = cms.EDFilter("Pythia8TFuncPtGun",
     PGunParameters = cms.PSet(
-        MaxPt = cms.double(150.0),
-        MinPt = cms.double(19.0),
-        ParticleID = cms.vint32(443),
-        MaxY  = cms.double(3.0),
+        MaxPt  = cms.double(150.0),
+        MinPt  = cms.double(19.0),
+        MaxEta = cms.double(3.0),
+        MinEta = cms.double(-3.0),
         MaxPhi = cms.double(3.14159265359),
-        MinY = cms.double(-3.0),
-        AddAntiParticle = cms.bool(False),
         MinPhi = cms.double(-3.14159265359),
-        TFunction_string = cms.string('x*((1.+x*x/((3.274-2.)*3.221))^(-3.274))'),
-        TFunction_min = cms.double(19.),
-        TFunction_max = cms.double(150.),
+        ParticleID       = cms.vint32(443),
+        AddAntiParticle  = cms.bool(False),
+        TFunction_string = cms.string('x*((1.+x*x/((3.274-2.)*3.221))^(-3.274))')
     ),
     pythiaPylistVerbosity = cms.untracked.int32(0),
-    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    pythiaHepMCVerbosity  = cms.untracked.bool(False),
     maxEventsToPrint = cms.untracked.int32(0),
     comEnergy = cms.double(13600.0),
     PythiaParameters = cms.PSet(
